1d3a636638baefda97ce4b804592a0cacedef7cf
Park-ma <park-ma@qq.com>
2021-01-20 21:59:45 +0800

    init skipList

---
+++ include/Kruskal.h
@@ -0,0 +1,8 @@
+
+#ifndef LEARN_ALGORITHMS_KRUSKAL_H
+#define LEARN_ALGORITHMS_KRUSKAL_H
+#include "UnionFind.h"
+namespace learn_al{
+    class
+}
+#endif //LEARN_ALGORITHMS_KRUSKAL_H
---
+++ include/SkipList.h
@@ -0,0 +1,9 @@
+#ifndef LEARN_ALGORITHMS_SKIPLIST_H
+#define LEARN_ALGORITHMS_SKIPLIST_H
+namespace learn_al{
+    template<typename T>
+    class skipList{
+
+    };
+}
+#endif //LEARN_ALGORITHMS_SKIPLIST_H
